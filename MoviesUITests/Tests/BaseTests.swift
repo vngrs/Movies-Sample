a63d909888ea3c2b6965781ecb0c65cff7c5891b
gunesmes <gunesmes@gmail.com>
2020-06-12 12:32:43 +0300

    pom implemented, test for homescreen added

---
+++ MoviesUITests/Tests/BaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  BaseTests.swift
+//  MoviesUITests
+//
+//  Created by MESUT GUNES on 12.06.2020.
+//  Copyright © 2020 VNGRS. All rights reserved.
+//
+
+import XCTest
+
+class BaseTest: XCTestCase {
+    let basePage = BasePage()
+    let app = BasePage().app
+
+    let defaultLoadingTime = 15.0
+    let defaultWaitTime = 5.0
+
+    override func setUp() {
+        app.launch()
+        app.launchArguments = ["enable-testing"]
+        continueAfterFailure = false
+        
+        waitForPageLoading(element: basePage.header)
+    }
+
+    override func tearDown() {
+        app.terminate()
+    }
+
+    func waitForPageLoading(element: XCUIElement) {
+        XCTAssertTrue(element.waitForExistence(timeout: defaultLoadingTime))
+    }
+
+}
