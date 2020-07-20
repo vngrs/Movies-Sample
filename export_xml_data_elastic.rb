b8fd4bd5a0463bc2cca175833537d1ce8cb54895
gunesmes <gunesmes@gmail.com>
2020-07-20 10:00:46 +0300

    test(Tests): enhancement of nightly checks
    test(Tests): .log ignored and removed
    
    test(Tests): adding unittest scheme
    
    test(Tests): removed unnecessary files

---
+++ export_xml_data_elastic.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+
+require 'rexml/document'
+require "nokogiri"
+require "uri"
+require "net/http"
+
+
+ELASTIC_URL = ENV["ELASTIC_URL"]
+ELASTIC_API_KEY = ENV["ELASTIC_API_KEY"]
+PROJECT = ARGV[0]
+TEST_RESULT_DIR = ARGV[1]
+TEST_TYPE = ARGV[2]
+
+TIME_NOW = Time.now.strftime('%Y-%m-%dT%H:%M:%S')
+
+
+def export_data()
+	# some test produce more than one result file
+	Dir.glob("#{TEST_RESULT_DIR}/*.xml") do |xml_file|
+		doc = File.open(xml_file) { |f| Nokogiri::XML(f) }
+		doc.search("testsuite").each do |test_result| 
+			send_data test_result
+		end
+	end
+end
+
+def send_data(line)
+	data =  {}
+
+	status = "success"
+	if line.attr("failures").to_i > 0
+		status = "failed"
+	elsif line.attr("errors").to_i > 0
+		status = "error"
+	end
+
+	timestamp = TIME_NOW
+
+	data.store("testProject", "#{PROJECT}-#{TEST_TYPE}-test")
+	data.store("tests", line.attr("tests").to_i)
+	data.store("name", line.attr("name"))
+	data.store("time", line.attr("time").to_i)
+	data.store("errors", line.attr("errors").to_i)
+	data.store("failures", line.attr("failures").to_i)
+	data.store("timeString", timestamp + "M")
+	data.store("analysedAt", timestamp)
+	data.store("success", line.attr("tests").to_i - (line.attr("errors").to_i + line.attr("failures").to_i))
+
+	url = URI(ELASTIC_URL)
+	http = Net::HTTP.new(url.host, url.port);
+	request = Net::HTTP::Post.new(url)
+	request["api_key"] = ELASTIC_API_KEY
+	request["Content-Type"] = "application/json"
+	request.body = data.to_s.gsub("=>", ":")
+
+	response = http.request(request)
+
+	print("  - new data exported: #{response.code}    data: #{data}\n")
+end
+
+if __FILE__ == $0
+    export_data()
+end
